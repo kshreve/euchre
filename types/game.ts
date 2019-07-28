f7d59da991e9be2ee3e1daf6b5f4785840efc21d
Kevin Shreve <kshreve@gmail.com>
2019-07-28 13:28:46 -0400

    types

---
+++ types/game.ts
@@ -0,0 +1,10 @@
+export interface Game {
+    team1AId: number,
+    team1BId: number,
+    team2AId: number,
+    team2BId: number,
+    team1Points: number,
+    team2Points: number,
+    team1Winner: boolean,
+    team2Winner: boolean,
+}
---
+++ types/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number,
+    gamesPlayed: number,
+    gamesWon: number,
+    winPercent: number,
+    totalPoints: number,
+    pointsPerGame: number,
+    lastDatePlayed: Date,
+}
